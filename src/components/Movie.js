6743f53108902837492720b7df17918a216e67b8
derrick b <dbozich1@gmail.com>
2018-11-04 18:28:20 +0000

    automatically backed up by learn

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,11 +2,12 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 
-const Movie = () => {
+const Movie = ({match}) => {
 
   return(
     <div>
       <h1>Movie Component</h1>
+
     </div>
   )
 }
