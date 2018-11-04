c4a92eddbf978523eaf0241fb9720309dac9617c
derrick b <dbozich1@gmail.com>
2018-11-04 18:10:11 +0000

    automatically backed up by learn

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { 
-  BrowserRouter as Router, 
-  Route 
+import {
+  BrowserRouter as Router,
+  Route
 } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import MoviesPage from './MoviesPage';
@@ -13,11 +13,11 @@ class App extends Component {
         <div>
           <NavBar />
           <Route exact path="/" render={() => <div>Home</div>} />
-          <Route path='/movies' component={MoviesPage}/>
+          <Route exact path='/movies' component={MoviesPage}/>
         </div>
       </Router>
     );
   }
 }
- 
+
 export default App;
