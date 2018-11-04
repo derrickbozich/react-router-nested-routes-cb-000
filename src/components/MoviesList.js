fd11f11c39d614f6a0c517f8d8bebb8b59504419
derrick b <dbozich1@gmail.com>
2018-11-04 17:48:56 +0000

    automatically backed up by learn

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
 const MoviesList = ({movies}) => {
-  debugger
+  const moviesList = movies.map(({id, title}) => {
+    <li><Link to={/movies/`${id}`}</li>
+  })
   return(
     <div>
       <h1>Movies List</h1>
